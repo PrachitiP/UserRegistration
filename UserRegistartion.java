35f08520d558036862f68e44f43edadb94cbd54c
PrachitiP <patilprachiti35@gmail.com>
2020-11-24 22:53:06 +0530

    Pattern for Last Name

--- UserRegistartion.java
+++ UserRegistartion.java
@@ -10,9 +10,13 @@ public class UserRegistartion {
 		System.out.println("Enter First Name:");
 		String fName = sc.nextLine();
 		u.firstName(fName);
+		System.out.println("Last Name");
+		String lName = sc.nextLine();
+		u.lastName(lName);
 		sc.close();
 	}
 	
+	/*UC1*/
 	public void firstName(String str)
 	{
 		String regexforFirstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
@@ -24,6 +28,21 @@ public class UserRegistartion {
 		{
 			System.out.println("First Name is invalid");
 		}
+	}
+	
+	/*UC2*/
+	public void lastName(String str)
+	{
+		String regexforLastName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+		if(Pattern.matches(regexforLastName, str))
+		{
+			System.out.println("Last Name is valid");
+		}
+		else
+		{
+			System.out.println("Last Name is invalid");
+		}
+		
 	}
 	
 	
