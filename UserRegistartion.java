bc902f4fcd4b1f8d65f7d1804aad1217c97ab638
PrachitiP <patilprachiti35@gmail.com>
2020-11-24 22:43:50 +0530

    User Registration Pattern

---
+++ UserRegistartion.java
@@ -0,0 +1,30 @@
+package BridgeLabz.UserRegistration;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class UserRegistartion {
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		UserRegistartion u = new UserRegistartion();
+		System.out.println("Enter First Name:");
+		String fName = sc.nextLine();
+		u.firstName(fName);
+		sc.close();
+	}
+	
+	public void firstName(String str)
+	{
+		String regexforFirstName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+		if(Pattern.matches(regexforFirstName, str))
+		{
+			System.out.println("First Name is valid");
+		}
+		else
+		{
+			System.out.println("First Name is invalid");
+		}
+	}
+	
+	
+}
